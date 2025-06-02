c23d5069802eb06f643ba099c423ed03500bc0a3
Niel22 <niel2264@gmail.com>
2025-06-02 12:05:44 +0100

    Animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { motion } from 'framer-motion';
 import { useState } from 'react';
 import { DiSizzlejs } from 'react-icons/di';
 import { HiMenuAlt3, HiOutlineX } from 'react-icons/hi';
+import { fadeIn } from '../utils/motion';
+
 
 const Navbar = () => {
 
@@ -16,47 +19,77 @@ const Navbar = () => {
   ];
 
   return (
-    <div className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm border-b border-gray-100 shadwo-sm z-100'>
+    <motion.div 
+    variants={fadeIn('down', 0.2)}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: true }}
+    className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm border-b border-gray-100 shadwo-sm z-100'>
       <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16'>
-        {/* Logo */}
-        <div className='flex items-center gap-1 cursor-pointer'>
-          <a href="/"><DiSizzlejs className='text-red-800' size={50} /></a>
-        </div>
+        
+        <motion.div 
+          variants={fadeIn('right', 0.3)}
+        className='flex items-center gap-1 cursor-pointer'>
+          <motion.a
+            whileHover={{ scale: 1.1 }}
+          href="/"><DiSizzlejs className='text-red-800' size={50} /></motion.a>
+        </motion.div>
 
-        {/* Mbile menu btn */}
-        <button className='bg-transparent md:hidden px-2 py-2' onClick={() => setIsMenuOpen(!isMenuOpen)}>
+
+        <motion.button
+          variants={fadeIn('left', 0.3)}
+          className='bg-transparent md:hidden px-2 py-2' onClick={() => setIsMenuOpen(!isMenuOpen)}>
           {isMenuOpen ? <HiOutlineX size={30}/> : <HiMenuAlt3 size={30} />}
-        </button>
+        </motion.button>
 
-        {/* Desktop nav */}
-        <div className='hidden md:flex gap-10'>
+
+        <motion.div
+          variants={fadeIn('down', 0.3)}
+          className='hidden md:flex gap-10'>
           {menuItem.map((item, index) => (
-            <a onClick={() => setActiveLink(item.href)} key={index} href={item.href} className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all ${activeLink === item.href ? "text-blue-600 after:w-full" : "text-gray-600 hover:text-gray-900"}`}>{item.label}</a> 
+            <motion.a 
+              variants={fadeIn('down', 0.1 * (index + 1))}
+            onClick={() => setActiveLink(item.href)} key={index} href={item.href} className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all ${activeLink === item.href ? "text-blue-600 after:w-full" : "text-gray-600 hover:text-gray-900"}`}>{item.label}</motion.a> 
           ))}
-        </div>
-
-        {/* Button */}
-        <button className='hidden md:block bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100'>
+        </motion.div>
+        
+        <motion.button 
+          variants={fadeIn('left', 0.3)}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        className='hidden md:block bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100'>
           <a href="#newsletter">Get in touch</a>
-        </button>
+        </motion.button>
       </div>
 
-      {/* Mobile Menu Items */}
+
       {isMenuOpen && (
-        <div className='md:hidden bg-white border-t border-gray-100 py-4'>
-          <div className='container mx-auto px-4 space-y-4'>
+        <motion.div 
+          variants={fadeIn('down', 0.2)}
+          initial="hidden"
+          whileInView="show"
+        className='md:hidden bg-white border-t border-gray-100 py-4'>
+          <motion.div 
+            variants={fadeIn('down', 0.3)}
+          className='container mx-auto px-4 space-y-4'>
             {menuItem.map((item, index) => (
-              <a onClick={() => {setActiveLink(item.href); setIsMenuOpen(false)}} 
-              className={`block text-sm font-medium py-2 ${activeLink === item.href ? "text-blue-600" : "text-gray-600 hover:text-gray-900"}`} href={item.href} key={index}>{item.label}</a>
+              <motion.a
+                variants={fadeIn('right', 0.1 * (index + 1))}
+              onClick={() => {setActiveLink(item.href); setIsMenuOpen(false)}} 
+              className={`block text-sm font-medium py-2 ${activeLink === item.href ? "text-blue-600" : "text-gray-600 hover:text-gray-900"}`} href={item.href} key={index}>{item.label}</motion.a>
             ))}
 
-            <button className='w-full bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100'>
+            <motion.button 
+            variants={fadeIn('up', 0.4)}
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.95 }}
+            className='w-full bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100'>
               <a href="#newsletter">Get in touch</a>
-            </button>
-          </div>
-        </div>
+            </motion.button>
+          </motion.div>
+        </motion.div>
       )}
-    </div>
+    </motion.div>
   )
 }
 
